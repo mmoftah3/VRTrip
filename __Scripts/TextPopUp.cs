1df63f6a78b4e5bf6a1f19806a17d692deb0abbf
mmoftah3 <71080372+mmoftah3@users.noreply.github.com>
2021-05-02 16:54:30 -0400

    Add files via upload

---
+++ __Scripts/TextPopUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPopUp : MonoBehaviour
+{
+    public Transform PlayerObject;
+    public float DistanceToView = 10f;
+    private TextMesh myTextMesh;
+    private string MyOriginalText;
+
+    void Start()
+    {
+        myTextMesh = GetComponent<TextMesh>();
+        MyOriginalText = myTextMesh.text;
+    }
+    void Update()
+    {
+        if (Vector3.Distance(PlayerObject.position, transform.position) <= DistanceToView)
+        {
+            myTextMesh.text = MyOriginalText;
+        }
+        else
+        {
+            myTextMesh.text = "";
+        }
+    }
+}
+
